7d65f600cc0d19d2175b89a81f5d7b5ced5b0bc9
Thomas-Basadonne <thommybasa@gmail.com>
2023-03-28 18:33:16 +0200

    card style

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,16 +4,17 @@
 <div class="container my-5">
     <h1 class="fs-2 fw-900 badge bg-primary text-white">CURRENT SERIES</h1>
 
-    <div class="row row-cols-4">
+    <div class="row row-cols-6 gy-4 my-2">
         @foreach ($comics as $comic)
-        <div class="my-card">
-            <div class="card-details p-5 my-3">
-                {{-- <img src="{{ $comic['thumb'] }}" alt=""> --}}
-                <img src="https://picsum.photos/400/500" alt="">
-                <p>{{ $comic['series'] }}</p>
+        <div class="card">
+            <img src="https://picsum.photos/500/500" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{ $comic['series'] }}</h5>
             </div>
         </div>
         @endforeach
     </div>
 </div>
 @endsection   
+
+
